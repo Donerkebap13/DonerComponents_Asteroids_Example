fde221acb78418622baa17301ee1dba49e1888b8
Donerkebap13 <donerkebap13@gmail.com>
2018-01-13 15:58:46 +0100

    - travis fix

--- generate_unix.sh
+++ generate_unix.sh
@@ -2,14 +2,14 @@
 
 ROOT=`pwd`
 echo ${ROOT}
-set PROJECT_FOLDER="${ROOT}\project"
-echo ${PROJECT_FOLDER}
+set PROJECT_PATH="${ROOT}/project"
+echo ${PROJECT_PATH}
 EXTRA_CMAKE_PARAMETERS=$1
 
-if [ ! -d ${PROJECT_FOLDER} ]; then
-    mkdir -p ${PROJECT_FOLDER}
+if [ ! -d ${PROJECT_PATH} ]; then
+    mkdir -p ${PROJECT_PATH}
 fi
 
-cd ${PROJECT_FOLDER}
+cd ${PROJECT_PATH}
 
-cmake "..\asteroids" -DMAX_ENTITIES=4096 -DMAX_TAGS=64 -DBUILD_SHARED_LIBS=0 ${EXTRA_CMAKE_PARAMETERS}
+cmake "../asteroids" -DMAX_ENTITIES=4096 -DMAX_TAGS=64 -DBUILD_SHARED_LIBS=0 ${EXTRA_CMAKE_PARAMETERS}
